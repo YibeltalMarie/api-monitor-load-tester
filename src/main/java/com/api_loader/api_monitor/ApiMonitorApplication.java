897aca2450a816cd7e5d0ba83e87d4b432426c78
Yerosan Bekele <lebrondee20@gmail.com>
2026-05-17 17:20:10 +0300

    initial commit

---
+++ src/main/java/com/api_loader/api_monitor/ApiMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.api_loader.api_monitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiMonitorApplication.class, args);
+	}
+
+}
